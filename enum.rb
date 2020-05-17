a7fe20d775b61973006eabbc86ea960d5d5073b1
paul <ajuzepaul>
2020-05-18 01:19:08 +0300

    my_each works with range

--- enum.rb
+++ enum.rb
@@ -1,17 +1,31 @@
 module Enumerable
   def my_each
-    for i in 0...self.length
-      yield self[i] if self.kind_of?(Array)
-      yield self.keys[i], self[keys[i]] if self.kind_of?(Hash)
+    if self.kind_of?(Range)
+      range_arr = self.to_a
+      obj_len = range_arr.length
+      if obj_len == nil
+        obj_len = +1.0/0.0
+      end
+    else
+      obj_len = self.length
     end
-    self
+    for i in 0...obj_len
+      yield self[i] if self.kind_of?(Array)
+      yield keys[i], values[i] if self.kind_of?(Hash)
+      yield range_arr[i] if self.kind_of?(Range)
+    end
+    return self
   end
 end
+
+
+h = (1..)
+h.my_each {|val| puts val}
+=begin
 ex = {"birthday" => 1994, "day" => 7, "status" => "dating"}
 ey = ex.keys
 ex.my_each {|item, item_value| puts item_value}
 
-=begin
 ex.my_each do |yul, rata|
   puts yul
 end
