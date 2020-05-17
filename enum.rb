4cacb22695fa11049ac93181d31625f837d7a63a
paul <ajuzepaul>
2020-05-18 01:54:02 +0300

    removed rubocop loop fix

--- enum.rb
+++ enum.rb
@@ -3,7 +3,7 @@ module Enumerable
     return self if block_given? != true
 
     j = 0
-    each do |i|
+    for i in self
       yield i if is_a?(Array) || is_a?(Range)
       yield keys[j], values[j] if is_a?(Hash)
       j += 1
