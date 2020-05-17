a0608e018db50987e4584ac775f706b315466ba6
paul <ajuzepaul>
2020-05-18 01:46:41 +0300

    fix linter errors & removed comments

--- enum.rb
+++ enum.rb
@@ -1,23 +1,13 @@
 module Enumerable
   def my_each
     return self if block_given? != true
+
     j = 0
-    for i in self
-      yield i if (self.kind_of?(Array) || self.kind_of?(Range))
-      yield keys[j], values[j] if self.kind_of?(Hash)
-      j+=1
+    each do |i|
+      yield i if is_a?(Array) || is_a?(Range)
+      yield keys[j], values[j] if is_a?(Hash)
+      j += 1
     end
-    return self
+    self
   end
 end
-
-=begin
-rang = ("a".."v")
-rang.my_each {|val| puts val}
-
-arr = [1,5,5,9,7,0,1]
-arr.my_each {|val| puts val}
-
-dict =  {"birthday" => 1850, "day" => 7, "status" => "dead"}
-dict.my_each {|item, val| puts "value for #{item} = #{val}"}
-=end
