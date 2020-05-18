719c696f0937a2ed280122fb3f81fedf89f3e4f7
paul <ajuzepaul>
2020-05-18 22:16:00 +0300

    my_each_with_index works with hashes

--- enum.rb
+++ enum.rb
@@ -2,11 +2,11 @@ module Enumerable
   # rubocop:disable Style/For
   def my_each
     return to_enum(:my_each) if block_given? != true
-    
+
     j = 0
     for i in self
       yield i if is_a?(Array) || is_a?(Range) || (is_a?(Hash) && (Proc.new).arity == 1)
-      yield keys[j], values[j] if is_a?(Hash) && (Proc.new).arity == 2
+      yield keys[j], values[j] if is_a?(Hash) && (Proc.new).arity > 1
       j += 1
     end
     self
@@ -14,11 +14,11 @@ module Enumerable
   # rubocop:enable Style/For
 
   def my_each_with_index
-    return to_enum(:my_each) if block_given? != true
+    return to_enum(:my_each_with_index) if block_given? != true
 
     j = 0
-    self.my_each do |i, hash_value|
-      yield i, j if is_a?(Array) || is_a?(Range)
+    self.my_each do |i|
+      yield i, j
       j+=1
     end
   end
@@ -29,7 +29,6 @@ pop = {
   :driver => 3,
   age: 90
 }
-
 #pop.my_each_with_index {|item, value| puts item}
 #pop.my_each {|item| puts item}
-pop.my_each {|item, value| puts item}
+pop.my_each_with_index {|item, value| puts item}
