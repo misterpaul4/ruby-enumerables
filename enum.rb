db787afbc78600dff33f9f504ce5ba0452dc6019
paul <ajuzepaul>
2020-05-20 01:13:43 +0300

    fix linter errors

--- enum.rb
+++ enum.rb
@@ -82,16 +82,16 @@ module Enumerable
     j = 0
     if param.nil? != true
       my_each do |i|
-        counter = counter + 1 if param == i
+        counter += 1 if param == i
       end
     elsif block_given?
       my_each do |i|
         counter = j if yield i
-        j+=1
+        j += 1
       end
     else
-      my_each do |i|
-        j+=1
+      my_each do
+        j += 1
         counter = j
       end
     end
@@ -99,7 +99,6 @@ module Enumerable
     counter
   end
 end
-
 
 # p [nil, true, 99].my_any?
 # p %w[ant bear cat].my_all? { |word| word.length < 5 }
