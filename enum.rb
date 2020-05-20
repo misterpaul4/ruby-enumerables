37a0755861791f288e8ce9d830e18ec5a7efd009
paul <ajuzepaul>
2020-05-21 01:29:57 +0300

    my_inject can receive a second argument when block is not given

--- enum.rb
+++ enum.rb
@@ -114,6 +114,7 @@ module Enumerable
  def my_inject(accum = nil, symb = nil, &block)
    object_passed = self.is_a?(Range) ? self.to_a : self
 
+
    if block_given?
      j = 1
      j = 0 unless accum.nil?
@@ -125,10 +126,32 @@ module Enumerable
          j += 1
        end
 
-     elsif symb.is_a?(String) || symb.is_a?(Symbol)
+     else symb.is_a?(String) || symb.is_a?(Symbol)
        symb = symb.to_s
        object_passed.my_each do |i|
          accum = eval "#{accum} #{symb} #{i}"
+       end
+     end
+
+   else
+     j = 0
+     j = 1 if symb.nil?
+     unless accum.nil?
+       if (accum.is_a?(Symbol) || accum.is_a?(String)) && symb.nil?
+         symb = accum.to_s
+         accum = first
+         while j < object_passed.length
+           #p "acummulator = #{accum} item = #{object_passed[j]}"
+           accum = eval "#{accum} #{symb} #{object_passed[j]}"
+           j += 1
+         end
+
+       elsif accum.is_a?(Integer) && (symb.is_a?(Symbol) || symb.is_a?(String))
+         while j < object_passed.length
+           p "acummulator = #{accum} item = #{object_passed[j]}"
+           accum = eval "#{accum} #{symb} #{object_passed[j]}"
+           j += 1
+         end
        end
      end
    end
@@ -139,8 +162,10 @@ end
 
 puts "MINE"
 #p [5, 7, 10, 20].my_inject(1, :/) { |product, n| n + product }
+p (5..10).my_inject(5, "*")
 puts "\n\n\nRUBY"
 #p [5, 7, 10, 20].inject(1, :/) { |product, n| n + product }
+p (5..10).inject(5, "*")
 
 
 #p [55555,7,10].my_inject { |sum, n| sum / n }
