5513dc7b26afcc87755319d1ebc485cda01d7656
paul <ajuzepaul>
2020-05-23 00:27:02 +0300

    my_any can accept argument
    - my_any passes all test

--- enum.rb
+++ enum.rb
@@ -53,12 +53,31 @@ module Enumerable
     true
   end
 
-  def my_any?
-    return true if block_given? != true && include?(false || nil)
+  def my_any?(*param)
 
     if block_given?
       my_each do |i|
         return true if yield i
+      end
+    else
+      unless param.empty? # when parameter is not empty
+        if param[0].is_a?(Class)
+          puts "it is a class"
+          my_each do |i|
+            return true if i.is_a?(param[0])
+          end
+        elsif param[0].is_a?(Regexp)
+          #puts "it is a regular expression"
+          my_each do |i|
+            return true if i === (param[0])
+          end
+        else
+          my_each do |i|
+            return true if i.eql? param[0]
+          end
+        end
+      else
+        return true if include?(false || nil)
       end
     end
 
@@ -160,12 +179,24 @@ module Enumerable
  end
 end
 
-puts "MINE"
+p 'my_any?'
+#p %w[ant bear cat].my_any? { |word| word.length >= 3 } #=> 1. true
+#p %w[ant bear cat].my_any? { |word| word.length >= 4 } #=> 2. true
+#p %w[ant bear cat].my_any?(/d/) #=> 3. false
+#p [nil, true, 99].my_any?(Integer) #=> 4. true
+#p [nil, true, 99].my_any? #=> 5. true
+#p [].my_any? #=> 6. false
+#p [1, 2, 3, 's'].my_any?(String) #=> 7. true
+#p [1, 2, 3, 's'].my_any?(Numeric) #=> 8. true
+#p [1, 2, 3].my_any?(String) #=> 9. false
+#p [1, 2].my_any?(1) # 10. true
+#p [1, 1].my_any?(1) # 11. true
+#puts "MINE"
 #p [5, 7, 10, 20].my_inject(1, :/) { |product, n| n + product }
-p (5..10).my_inject(5, "*")
-puts "\n\n\nRUBY"
+#p %w[ant bear cat 5].my_any?(/d/)
+#puts "\n\n\nRUBY"
 #p [5, 7, 10, 20].inject(1, :/) { |product, n| n + product }
-p (5..10).inject(5, "*")
+#p %w[ant bear cat 5].any?(/s/)
 
 
 #p [55555,7,10].my_inject { |sum, n| sum / n }
